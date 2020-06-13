8170d73440fc1ea4d5dcd6e24adeb839f6d45461
vamshireddy01 <m.vamshikrishna8@gmail.com>
2020-06-13 17:29:58 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -50,6 +51,7 @@ class Main extends Component{
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
                     <Route path="/menu/:dishId" component={ DishWithId } />
+                    <Route exact path="/aboutus" component={ () => <About leaders={this.state.leaders}/> } />
                     <Route exact path="/contactus" component={ Contact } />
                     <Redirect to="/home" />
                 </Switch>
